f49e2c8780ad00a3294b9eca135e19d6f6c5a2d4
o-tomox <satoshi.o.xxx@gmail.com>
2014-11-20 20:30:23 +0900

    昨日の日付で実行するように修正

--- mysterious_generator/count.py
+++ mysterious_generator/count.py
@@ -2,7 +2,7 @@
 
 import glob
 import smtplib
-from datetime import datetime
+import datetime
 from email.MIMEText import MIMEText
 from email.Header import Header
 from email.Utils import formatdate
@@ -14,22 +14,30 @@ def count_docs(day=None):
     day（日付）に生成された怪文書の数を数える関数
     '''
     if day is None:
-        day = datetime.now().strftime("%Y%m%d")
+        day = datetime.date.today()
 
-    target_file = "{0}/{1}*.png".format(settings.img_path, day)
+    day_str = day.strftime("%Y%m%d")
+
+    target_file = "{0}/{1}*.png".format(settings.img_path, day_str)
 
     files = glob.glob(target_file)
 
     return len(files)
 
-def mail(mail_to, num):
+def mail(mail_to, num, day=None):
     u'''
     指定されたメールアドレスに送信する関数
     '''
+    if day is None:
+        day = datetime.date.today()
+    day_str = day.strftime("%Y/%m/%d")
+
     mail_from = "info@o-tomox.com"
     subject = u"怪文書ジェネレータ 定期メール"
-    body = u"本日、生成された怪文書は {0} 件です。".format(num)
+    body = u"{0}に生成された怪文書は {1} 件です。".format(day_str, num)
+
     msg = create_message(mail_from, mail_to, subject, body, "ISO-2022-JP")
+
     send(mail_from, mail_to, msg)
 
 def create_message(from_addr, to_addr, subject, body, encoding):
@@ -56,8 +64,9 @@ def main():
     u'''
     メイン関数
     '''
-    num = count_docs()
-    mail(settings.mail_address, num)
+    yesterday = datetime.date.today() - datetime.timedelta(1)
+    num = count_docs(yesterday)
+    mail(settings.mail_address, num, yesterday)
 
 
 if __name__ == "__main__":
