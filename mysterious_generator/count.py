983b4729871665da4ef8b9f73e549c20ea6b80b0
o-tomox <satoshi.o.xxx@gmail.com>
2014-11-20 20:22:17 +0900

    メールを送る処理を実装

--- mysterious_generator/count.py
+++ mysterious_generator/count.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import glob
+import smtplib
 from datetime import datetime
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import formatdate
 
 import settings
 
@@ -22,7 +26,31 @@ def mail(mail_to, num):
     u'''
     指定されたメールアドレスに送信する関数
     '''
-    pass
+    mail_from = "info@o-tomox.com"
+    subject = u"怪文書ジェネレータ 定期メール"
+    body = u"本日、生成された怪文書は {0} 件です。".format(num)
+    msg = create_message(mail_from, mail_to, subject, body, "ISO-2022-JP")
+    send(mail_from, mail_to, msg)
+
+def create_message(from_addr, to_addr, subject, body, encoding):
+    u'''
+    日本語のメッセージを生成する関数
+    '''
+    msg = MIMEText(body, "plain", encoding)
+    msg["Subject"] = Header(subject, encoding)
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+    msg["Date"] = formatdate()
+    return msg
+
+def send(from_addr, to_addr, msg):
+    u'''
+    実際にメールを送信する関数
+    '''
+    s = smtplib.SMTP()
+    s.connect()
+    s.sendmail(from_addr, [to_addr], msg.as_string())
+    s.close()
 
 def main():
     u'''
