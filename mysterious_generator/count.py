c1da38c68c2031022a161022a9a5b3fb41438213
o-tomox <satoshi.o.xxx@gmail.com>
2014-11-20 19:42:46 +0900

    雛形の作成

---
+++ mysterious_generator/count.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+def count_docs(day=None):
+    u'''
+    day（日付）に生成された怪文書の数を数える関数
+    '''
+    pass
+
+def mail(mail_to, num):
+    u'''
+    指定されたメールアドレスに送信する関数
+    '''
+    pass
+
+def main():
+    u'''
+    メイン関数
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    main()
